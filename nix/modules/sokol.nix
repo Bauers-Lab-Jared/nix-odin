734be0a357049be44f7f957ccf8d8328dd17babd
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2025-02-24 16:07:00 -0500

    added slang option for sokol

--- nix/modules/sokol.nix
+++ nix/modules/sokol.nix
@@ -22,10 +22,25 @@ in {
       ]);
       default = [];
     };
+    slang = lib.mkOption {
+      type = types.listOf (types.enum [
+        "glsl410"
+        "glsl430"
+        "glsl300es"
+        "hlsl4"
+        "hlsl5"
+        "metal_macos"
+        "metal_ios"
+        "metal_sim"
+        "wgsl"
+      ]);
+      default = ["glsl430"];
+    };
   };
 
   config = lib.mkIf cfg.enable {
     nativeBuildInputStrs = ["sokol-shdc"];
+    extraEnvVars.SLANG = builtins.concatStringsSep ":" (lib.unique cfg.slang);
     buildInputStrs = map (moduleName: "odinLibs.sokol.${moduleName}") cfg.modules;
     libs.import = map (moduleName: "sokol.${moduleName}") cfg.modules;
   };
